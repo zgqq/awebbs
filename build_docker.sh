18261a14a7b83970c8990e5b451c54ac32d262c2
zgqq <zgqjava@gmail.com>
2017-05-20 13:42:17 +0800

    Fix build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,6 @@
 ./gradlew clean build
 docker rmi awebbs --force
+docker rmi $(docker images -f 'dangling=true' -q)
 docker build ../awebbs
 docker tag "$(docker images -f 'dangling=true' -q)" awebbs
 docker rm awebbs
